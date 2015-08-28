559e1a03f560a9b7bbeba44fcda12f5d2320df7b
Wu Zehui <zehuiwu@126.com>
2015-08-28 09:45:29 +0800

    add pascalsTriangle

--- algorithm/pascalsTriangle/pascalsTriangle.cpp
+++ algorithm/pascalsTriangle/pascalsTriangle.cpp
@@ -1,6 +1,9 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
+    vector< vector<int> > generate(int numRows) {
         vector< vector<int> > s(0);
         if(numRows > 0) {
             for(int i = 1; i <= numRows; i++) {
@@ -10,7 +13,7 @@ public:
                 else {
                     vector<int> t(i,1);
                     for(int j = 1; j <= i -2; j++){
-                        t[j] = s[i-1][j-1] + s[i-1][j];
+                        t[j] = s[i-2][j-1] + s[i-2][j];
                     }
                     s.push_back(t);
                 }
---
+++ algorithm/pascalsTriangle/test.cpp
@@ -0,0 +1,9 @@
+#include "pascalsTriangle.cpp"
+#include <vector>
+using namespace std;
+
+int main() {
+    Solution s;
+    vector< vector<int> > p = s.generate(5);
+    return 0;
+}
