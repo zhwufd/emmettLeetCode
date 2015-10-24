9a9c9eb3f943c9d120e87e2713b28ed400cd2492
emmettwu <zehuiwu@126.com>
2015-10-24 21:27:40 +0800

    modify readability

--- algorithm/removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
+++ algorithm/removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
@@ -1,24 +1,24 @@
 class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        if(nums.size() <= 1)
-            return nums.size();
-        int i = 1;
-        int j = 1;
-        while(i < nums.size() && nums[i] == nums[i-1])
-            i++;
-        while(i < nums.size()){
-                    int k = i+1;
-                    while(k < nums.size() && nums[k]== nums[k-1])
-                        k++;
-                    if(i!=j){
-                        int tmp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = tmp;
-                    }
-                    j++;
-                    i = k;
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            if(nums.size() <= 1)
+                return nums.size();
+            int i = 1;
+            int j = 1;
+            while(i < nums.size() && nums[i] == nums[i-1])
+                i++;
+            while(i < nums.size()){
+                int k = i+1;
+                while(k < nums.size() && nums[k]== nums[k-1])
+                    k++;
+                if(i!=j){
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                }
+                j++;
+                i = k;
+            }
+            return j;
         }
-        return j;
-    }
 };
